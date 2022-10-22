682874ff6261a52ba0e53a7bc3dda6360fc25d23
rvaras <rvaras@localhost.localdomain>
2022-10-22 17:52:41 -0300

    Adding basic logic for the button

--- app/src/main/java/com/example/androidactivitystate/MainActivity.kt
+++ app/src/main/java/com/example/androidactivitystate/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.androidactivitystate
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var name: String? = null
+
+    companion object {
+        const val MY_NAME = "MY_NAME"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Button is clicker
+        showYourName_button.setOnClickListener {
+            name = typeName_editText.text.toString()
+            displayName()
+        }
+    }
+
+    private fun displayName() {
+        Log.i("name", "Entered name is $name")
+        typedName_textView.text = name
     }
 }
