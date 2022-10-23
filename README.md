6f31b46cdc0d626ea5a4793403b07895b82988a7
rvaras <rvaras@localhost.localdomain>
2022-10-22 23:18:06 -0300

    Updated GUI and logic

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # androidActivityState
+
+# Overview
+The purpose of this project is to understand Activities States in Android.
+A simple project, where the user can type his name, then he can click a button and the entered text will be displayed in a textview component. When the phone is rotated the typed value must be kept.
+It is just for fun :)
--- app/src/main/java/com/example/androidactivitystate/MainActivity.kt
+++ app/src/main/java/com/example/androidactivitystate/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.androidactivitystate
 
+import android.view.inputmethod.InputMethodManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,15 +19,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Button is clicker
+        // Button is clicked
         showYourName_button.setOnClickListener {
-            name = typeName_editText.text.toString()
-            displayName()
+            name = typeYourName_TextInput.text.toString()
+            val myCopy: String? = name
+
+            // Get the current view
+            val view: View? = this.currentFocus
+
+            if (myCopy?.isEmpty() == true) {
+                Toast.makeText(applicationContext, "Name value is empty. Please type a name.", Toast.LENGTH_SHORT).show()
+                typedName_textView.text=""
+            } else {
+                if (view != null) {
+                    // Create a variable for input manager and init it
+                    val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+
+                    // Hide software keyboard
+                    inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0)
+                }
+                displayName()
+            }
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.i("State:","Entered onSaveInstanceState")
+        outState.putString(MY_NAME, name)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.i("State:","Entered onRestoreInstanceState")
+        name = savedInstanceState.getString(MY_NAME)
+        displayName()
+    }
+
+    // Will display the typed name in the textview component
     private fun displayName() {
-        Log.i("name", "Entered name is $name")
+        println("Entered name is $name")
         typedName_textView.text = name
     }
 }
